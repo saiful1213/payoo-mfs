f6a3cbf292b64ec89ce0399fa14b04660c22f3f5
saiful islam <saifulislamptcbd@gmail.com>
2024-09-19 00:37:15 -0700

    add money functionality completed

---
+++ js/home.js
@@ -0,0 +1,14 @@
+document.getElementById('addMoneyBtn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const pinNumber = document.getElementById('pin-number').value;
+    if (pinNumber === '1234') {
+        const amount = Number(document.getElementById('amount').value);
+        const balanceStr = document.getElementById('balance');
+        let balance = Number(balanceStr.innerText);
+        balance += amount;
+        balanceStr.innerText = balance;
+    }
+    else {
+        alert('wrong credintial')
+    }
+})
