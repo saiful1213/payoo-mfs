3b9fe6c8b56d6d7021b60010062a760a51085456
saiful islam <saifulislamptcbd@gmail.com>
2024-09-19 01:14:38 -0700

    cashOut functionality completed

--- js/cashOut.js
+++ js/cashOut.js
@@ -1,14 +1,15 @@
-document.getElementById('addMoneyBtn').addEventListener('click', (e) => {
+
+document.getElementById('cashOutBtn').addEventListener('click', (e) => {
     e.preventDefault();
     const pinNumber = document.getElementById('pin-number').value;
     if (pinNumber === '1234') {
         const amount = Number(document.getElementById('amount').value);
         const balanceStr = document.getElementById('balance');
         let balance = Number(balanceStr.innerText);
-        balance += amount;
+        balance -= amount;
         balanceStr.innerText = balance;
     }
     else {
-        alert('failed to add money')
+        alert('try again')
     }
 })
